1274020b0fbb1ad532fe546092bb92ab153af47e
levybeny <ySsdsp9j>
2018-04-19 18:15:33 +0300

    Ben commit

--- ANN.py
+++ ANN.py
@@ -3,33 +3,38 @@ import numpy as np
 
 # initialize the W and b parameters for the network model for each layer
 def initialize_parameters(layer_dims):
-    network_params={}
+    network_params = []
 
     #  W=[prev_layer_dim,current_layer_dim], b=[current_layer_dim]
     for i in range(1, len(layer_dims)):
-        network_params[i] = {}
-        network_params[i]["W"]=(np.random.randn(layer_dims[i-1],layer_dims[i]))
-        network_params[i]["b"]=(np.zeros(layer_dims[i],1))
+        layer_params = {"W": (np.random.randn(layer_dims[i], layer_dims[i-1])),
+                        "b": (np.zeros(layer_dims[i])).reshape(layer_dims[i], 1)}
+        network_params.append(layer_params)
 
     return network_params
 
+
 #
 def linear_forward(A, W, b):
-    Z=np.transpose(W)@A+b
-    linear_cache = {"A_prev":A,"W":W,"b":b,"Z":Z}
+    Z = W @ A + b
+    linear_cache = {"A_prev": A, "W": W, "b": b, "Z": Z}
     return Z, linear_cache
 
 
 def sigmoid(Z):
-    return 1/(1+np.exp(-Z)),Z
+    # res=np.zeros(Z.shape[1]).reshape(1,-1)
+    # for i in range(Z.shape[1]):
+    #     res[0,i]=1 / (1 + np.exp(-Z[0,i]))
+    # return res,Z
+    return 1 / (1 + np.exp(-Z)), Z
 
 
 def relu(Z):
-    return np.maximum(Z,np.zeros(Z.shape)) , Z
+    return np.maximum(Z, np.zeros(Z.shape)), Z
 
 
 def linear_activation_forward(A_prev, W, b, activation):
-    Z, linear_cache = linear_forward(A_prev , W, b)
+    Z, linear_cache = linear_forward(A_prev, W, b)
     if activation == "relu":
         A, _ = relu(Z)
     if activation == "sigmoid":
@@ -40,38 +45,40 @@ def linear_activation_forward(A_prev, W, b, activation):
 
 
 def L_model_forward(X, parameters):
-    A=X
-    caches_list=[]
+    A = X
+    caches_list = []
     activation = "relu"
 
     for i in range(len(parameters)):
-        if i == len(parameters)-1:
+        if i == len(parameters) - 1:
             activation = "sigmoid"
-        W, b = parameters[i][0],parameters[i][1]
+        W, b = parameters[i]["W"], parameters[i]["b"]
         A, linear_cache = linear_activation_forward(A, W, b, activation)
         caches_list.append(linear_cache)
 
     return A, caches_list
 
+
 def compute_cost(AL, Y):
-    m = float(Y.shape[1])
+    m = Y.shape[1]
     y = Y.reshape(m)
     al = AL.reshape(m)
-    toSum = y * np.log(al) + ((1-y) * np.log((1-al)))
-    cost = -1/m * np.sum(toSum)
+    toSum = y * np.log(al) + ((1 - y) * np.log((1 - al)))
+    cost = -1 / float(m) * np.sum(toSum)
     return cost
 
 
 ##### BACK PROPOGATION #####
 
-def linear_backward( dZ, cache):
-    m= cache["A_prev"].shape[1]
+def linear_backward(dZ, cache):
+    m = cache["A_prev"].shape[1]
 
     dA_prev = np.transpose(cache["W"]) @ dZ
-    dW = (1/m)*(dZ) @ np.transpose(cache["A_prev"])
-    db =(1/m)* np.sum(dZ, axis =1,keepdims=True)
+    dW = (1 / m) * dZ @ np.transpose(cache["A_prev"])
+    db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
     return dA_prev, dW, db
+
 
 def linear_activation_backward(dA, cache, activation):
     if activation == "relu":
@@ -81,10 +88,12 @@ def linear_activation_backward(dA, cache, activation):
 
     return linear_backward(dZ, cache)
 
-def relu_backward (dA, activation_cache):
-    if activation_cache["Z"] < 0:
-        return 0
-    return dA
+
+def relu_backward(dA, activation_cache):
+    dZ = np.array(dA, copy=True)
+    dZ[activation_cache["Z"] <= 0] = 0
+    return dZ
+
 
 def sigmoid_backward(dA, activation_cache):
     Z = activation_cache['Z']
@@ -92,18 +101,19 @@ def sigmoid_backward(dA, activation_cache):
     dZ = dA * res * (1 - res)
     return dZ
 
+
 #
 def L_model_backward(AL, Y, caches):
-    grads={}
-    dAL = -(np.divide(Y, AL)-np.divide(1-Y, 1-AL))
+    grads = {}
+    dAL = -(np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
 
     # last layer
-    dA, dW, db = linear_activation_backward(dAL, caches[len(caches)-1], "sigmoid")
-    grads["dA"+str(len(caches)-1)] = dA
+    dA, dW, db = linear_activation_backward(dAL, caches[len(caches) - 1], "sigmoid")
+    grads["dA" + str(len(caches) - 1)] = dA
     grads["dW" + str(len(caches) - 1)] = dW
     grads["db" + str(len(caches) - 1)] = db
 
-    for layer in range(len(caches)-2, 0 , -1):
+    for layer in range(len(caches) - 2, -1, -1):
         dA, dW, db = linear_activation_backward(dA, caches[layer], "relu")
         grads["dA" + str(layer)] = dA
         grads["dW" + str(layer)] = dW
@@ -111,41 +121,44 @@ def L_model_backward(AL, Y, caches):
 
     return grads
 
+
 def Update_parameters(parameters, grads, learning_rate):
     for i in range(len(parameters)):
         dW = grads["dW" + str(i)]
         db = grads["db" + str(i)]
-        parameters[i]["W"] = parameters[i]["W"]-learning_rate*dW
-        parameters[i]["b"] = parameters[i]["b"]-learning_rate*db
+        parameters[i]["W"] = parameters[i]["W"] - learning_rate * dW
+        parameters[i]["b"] = parameters[i]["b"] - learning_rate * db
 
     return parameters
 
 
 def L_layer_model(X, Y, layers_dims, learning_rate, num_iterations):
     # initialize ->L_model_forward -> compute_cost -> L_model_backward -> update parameters
-    parameters = []
+
     costs = []
-
+    layers_dims.insert(0, X.shape[0])
     initial_params = initialize_parameters(layers_dims)
+    parameters = initial_params
 
-    for i in range(1,num_iterations+1):
-        AL, caches_List = L_model_forward(X, initial_params)
+    for i in range(1, num_iterations + 1):
+        AL, caches_List = L_model_forward(X, parameters)
         cost = compute_cost(AL, Y)
-        if i% 100 == 0:
+        if i % 100 == 0:
             costs.append(cost)
         grads = L_model_backward(AL, Y, caches_List)
-        Update_parameters(initial_params, grads, learning_rate)
+        parameters=Update_parameters(initial_params, grads, learning_rate)
 
-    return parameters ,costs
+    return parameters, costs
+
 
 def Predict(X, Y, parameters):
     A, _ = L_model_forward(X, parameters)
     predict_y = [1 if prediction >= 0.5 else 0 for prediction in A.reshape(A.shape[1])]
 
-    #Calculate Acc
-    denominator = 0
-    for i, y in Y:
-        if y == predict_y[i]:
-            denominator+=1
+    # Calculate Accuracy
+    denominator = 0.0
+    for i in range(Y.shape[1]):
+        if Y[0,i] == predict_y[i]:
+            denominator += 1
 
-    return denominator/len(Y)
+    return denominator / Y.shape[1]
--- main.py
+++ main.py
@@ -2,20 +2,34 @@ from keras.datasets import mnist
 import ANN
 import numpy as np
 
+# extract data by given digits
+def get_data_by_digits(X,y,digit1,digit2):
+    indices = np.where(np.logical_or(y_train == digit1, y_train == digit2))
+    X_new = X_train[:, indices[0]]
+    y_new = y_train[indices[0]].reshape(1, -1)
+    y_new=(y_new == digit1).astype(np.float32)
+    return X_new,y_new
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-X_train
-
+# flatten the data
 X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] * X_train.shape[2]).T
 X_train = X_train.astype(np.float32)
 y_train = y_train.astype(np.float32)
-print(y_train.shape)
-# extract only 3 & 8 digits
-y_train,indices =np.where( (y_train==8) | (y_train==3))
-X_train_38=2
 
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1] * X_test.shape[2]).T
 X_test = X_test.astype(np.float32)
 y_test = y_test.astype(np.float32)
 
-# indices = np.ix_(train_y[])
+# extract only 3 & 8 digits
+X_train_38,y_train_38 = get_data_by_digits(X_train,y_train,3,8)
+X_test_38,y_test_38=get_data_by_digits(X_test,y_test,3,8)
+
+# extract only 7 & 9 digits
+X_train_79,y_train_79 = get_data_by_digits(X_train,y_train,7,9)
+X_test_79,y_test_79=get_data_by_digits(X_test,y_test,7,9)
+
+parameters, costs = ANN.L_layer_model(X_train_38, y_train_38, layers_dims=[20, 7, 5, 1], learning_rate=0.009,
+                                      num_iterations=100)
+acc=ANN.Predict(X_test_38,y_test_38,parameters)
+print(acc)
